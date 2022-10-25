a4727d304cad555226827eb3515b2f5f7144b792
Therakar <matteo.pau200@gmail.com>
2022-10-25 18:10:09 +0200

    esercizio mail completato

--- mail/js/main.js
+++ mail/js/main.js
@@ -5,17 +5,23 @@ const listEmail = ["Pino@gmail.com", "Gino@gmail.com", "Tino@gmail.com", "Lino@g
 // prompt per inserire la mail
 const newElement = prompt ("inserisci l'email:");
 
+// imposto una variabile sentinella 
 let trovato = false;
 // ciclo for
 for(let i = 0; i < listEmail.length; i++){
     let name = listEmail[i];
 
-    // se uno degli elementi è uguale a newElement ...
+    // se uno degli elementi è uguale a newElement la variabile sentinella viene cambiata di valore 
     if (name == newElement){
         trovato = true;
     }
-    // altrimenti ...
-    else{
-        
-    }
+}
+
+// se la variabile sentinella viene cambiata di valore allora viene stampato l'avviso che la mail esiste già
+if (trovato === true){
+    alert("L'email esiste già!");
+} 
+// se la variabile sentinella NON viene cambiata di valore allora viene stampato l'avviso che la mail è valida
+else if (trovato === false) {
+    alert("L'email è valida!")
 }
