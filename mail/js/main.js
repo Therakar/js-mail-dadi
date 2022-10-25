b2f549cbd56e397836da4ee268263cc2d76e2087
Therakar <matteo.pau200@gmail.com>
2022-10-25 17:24:03 +0200

    problema email

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,5 +1,23 @@
 "use strict"
+// array iniziale
+const listEmail = ["Pino@gmail.com", "Gino@gmail.com", "Tino@gmail.com", "Lino@gmail.com", "Dino@gmail.com", "Mino@gmail.com"]
 
-// elenco delle mail già presenti
-const listEmail = [ "Pino@gmail.com", "Gino@gmail.com", "Tino@gmail.com", "Lino@gmail.com", "Dino@gmail.com", "Mino@gmail.com"]
+// prompt per inserire la mail
+const newElement = prompt ("inserisci l'email:");
 
+// ciclo for
+for(let i = 0; i < listEmail.length; i++){
+    let name = listEmail[i];
+
+    // se uno degli elementi è uguale a newElement ...
+    if (name == newElement){
+        alert("Email già presente!");
+        console.log(listEmail[i]);
+    }
+    // altrimenti ...
+    else{
+        alert("Email disponibile!"); 
+        listEmail.push(newElement);
+        console.log(listEmail[i]);
+    }
+}
